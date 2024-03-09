8f3a7c768ddc6ef1867a139c496cdee3999f698f
ismark <akroutismail01@gmail.com>
2024-03-09 15:12:58 -0800

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -12,16 +12,15 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """constructor"""
         time_f = '%Y-%m-%dT%H:%M:%S.%f'
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    setattr(self, k, datetime.strptime(v, time_f))
+                    self.__dict__[k] = datetime.strptime(v, time_f)
                 else:
-                    setattr(self, k, v)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+                    self.__dict__[k] = v
         models.storage.new(self)
 
     def __str__(self):
