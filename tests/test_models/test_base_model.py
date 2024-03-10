5efb3e6d8ce370470eff37817c31cf58c0c1f9dd
ismark <akroutismail01@gmail.com>
2024-03-10 14:09:45 -0700

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """unittest module basemodel"""
 import unittest
 from models.base_model import BaseModel
@@ -8,6 +8,7 @@ class TestBaseModel(unittest.TestCase):
     """Define unittests for BaseModel"""
 
     def save_test(self):
+        """test save method"""
         my_model = BaseModel()
         created_at = my_model.created_at
         updated_at = my_model.save()
